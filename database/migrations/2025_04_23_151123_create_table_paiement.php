cb97196bcda789efdd6a6c3413516f1bd330232b
KEBE1607 <fatoukebecfpt@gmail.com>
2025-04-23 16:14:09 +0000

    Ajout

--- database/migrations/2025_04_23_151123_create_table_paiement.php
+++ database/migrations/2025_04_23_151123_create_table_paiement.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('table_paiement', function (Blueprint $table) {
             $table->id();
+            $table->string('mode_paiement');
             $table->timestamps();
         });
     }
