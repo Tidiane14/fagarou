b57f5988ee0a2aecc01da5de7e7f8454f1080f42
Pape-Momar-FALL <papemomarfallcfpt@gmail.com>
2025-04-23 15:53:00 +0700

    first commit

---
+++ app/Http/Controllers/ordonnance/ordonnanceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ordonnance;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ordonnanceController extends Controller
+{
+    //
+}
