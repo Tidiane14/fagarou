dbd8c865d89d79d3dcae1abd6f59e7d837866e9e
aidara2023 <122046394+aidara2023@users.noreply.github.com>
2025-04-25 11:37:58 +0000

    ok

--- database/migrations/2025_04_25_102538_create_stocks.php
+++ database/migrations/2025_04_25_102538_create_stocks.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_medicament');
+           // $table->unsignedBigInteger('id_fournisseur');
+            $table->string('quantite');
+            $table->string('date_ajout');
+            $table->string('date_modification')->nullable();
+            $table->string('statut')->default('disponible');
+            $table->foreign('id_medicament')->references('id')->on('medicaments')->onDelete('cascade');
+            //$table->foreign('id_fournisseur')->references('id')->on('fournisseurs')->onDelete('cascade');
+           // $table->string('fournisseur')->nullable();
+            //$table->string('date_expiration')->nullable();
+            //$table->string('prix_achat')->nullable();
+            //$table->string('prix_vente')->nullable();
+            //$table->string('date_ajout')->nullable();
+            //$table->string('date_modification')->nullable();
+            //$table->string('statut')->default('disponible');
+
             $table->timestamps();
         });
     }
