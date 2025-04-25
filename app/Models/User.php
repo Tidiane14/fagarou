5cad54934111296b0d1106ba89a565e73b02b212
Pape-Momar-FALL <papemomarfallcfpt@gmail.com>
2025-04-25 09:05:32 +0700

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'prenom',
+        'date_naissance',
+        'telephone',
+        'adresse',
+        'photo',
     ];
 
     /**
