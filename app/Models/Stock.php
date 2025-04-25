04a07d30ee9512127833c329706acf993693eabd
safietou29 <dsafietou52@gmail.com>
2025-04-25 12:07:45 +0000

    model_stock

---
+++ app/Models/Stock.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    protected $fillable = [
+        'id_pharmacie',
+        'quantity',
+        'date_mis_a_jour',
+    ];
+
+    public function pharmacie()
+    {
+        return $this->belongsTo(Pharmacie::class, 'id_pharmacie');
+    }
+    public function medicaments()
+    {
+        return $this->hasMany(Medicament::class, 'id_stock');
+    }
+}
