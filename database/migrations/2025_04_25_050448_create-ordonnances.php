49f1db633d8a1e6e2ac971f8cd7e4bd47a2552a9
Pape-Momar-FALL <papemomarfallcfpt@gmail.com>
2025-04-25 12:05:51 +0700

    modification

---
+++ database/migrations/2025_04_25_050448_create-ordonnances.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('concerner_ordonnance_medocs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('id_medoc');
+            $table->foreign('id_medoc')->references('id')->on('medocs')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
