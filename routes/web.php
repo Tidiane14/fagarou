daf0ed13013f5dab3bb68f751a78bd091c5adaca
SafitouTech <sokhnafatou04@gmail.com>
2025-05-02 11:59:05 +0000

    page admin

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
+
+Route::get('/administrateur', function () {
+    return view('admin.administrateur');
+})->name('login');
